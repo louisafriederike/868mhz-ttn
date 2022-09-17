57b4c6a6c34b872158d52f45fbb3f4ceb6aa89d4
louisafriederike <70212792+louisafriederike@users.noreply.github.com>
2022-09-17 18:10:25 +0200

    yyy

--- public/js/input.js
+++ public/js/input.js
@@ -14,7 +14,7 @@ socket.on('node5', function(sensorData){
     console.log("Dev Address",sensorData.dev_addr);
     console.log("Sensor Reading",reading); 
     // document.getElementById("file").value = sensor; 
-     if(reading==11 && nodeID == "260BD7A6"){
+     if(reading==11 && nodeID=="260BD7A6"){
        alert('node5 unlocked.');
      }
 });

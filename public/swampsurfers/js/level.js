828384cefbca9e22893543b2c1a66e802a69267b
louisafriederike <70212792+louisafriederike@users.noreply.github.com>
2023-03-07 16:06:29 +0100

    swampsurfer branch

---
+++ public/swampsurfers/js/level.js
@@ -0,0 +1,144 @@
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+var loc1 = new Audio('audio/loc1.WAV');
+var loc2 = new Audio('audio/loc2.WAV');
+var loc3 = new Audio('audio/loc3.wav');
+var loc4 = new Audio('audio/loc4.wav');
+var loc5 = new Audio('audio/loc5.wav');
+var inputString = $("#input").val();
+log1 = document.querySelector('#log1');
+log2 = document.querySelector('#log2');
+log3 = document.querySelector('#log3');
+log4 = document.querySelector('#log4');
+log5 = document.querySelector('#log5');
+dreamlog = document.querySelector('#dreamlog');
+back2 = document.querySelector('#back2');
+objective = document.querySelector('#objective');
+// r1 = document.querySelector('#rect13');
+// r2 = document.querySelector('#rect23');
+// r3 = document.querySelector('#rect35');
+// r4 = document.querySelector('#rect37');
+// r5 = document.querySelector('#rect27');
+// r6 = document.querySelector('#rect39');
+// r7 = document.querySelector('#rect41');
+// r8 = document.querySelector('#rect43');
+// r9 = document.querySelector('#rect19');
+// r10 = document.querySelector('#rect45');
+// r11 = document.querySelector('#rect47');
+// r12 = document.querySelector('#rect17');
+// r13 = document.querySelector('#rect49');
+// r14 = document.querySelector('#rect51');
+// r15 = document.querySelector('#rect15');
+
+
+// function levelOne(){
+//     console.log('lvl 1');
+//     inputString = '';
+//     dreamlog.style.display = "none";
+//     back2.style.display = "none";
+//     objective.style.display = "flex";
+//     log1.style.display = "none";
+//     log2.style.display = "none";
+//     log3.style.display = "none";
+//     log4.style.display = "none";
+//     log5.style.display = "none";
+//     document.querySelector('#rect13').style.fill = "black";
+//     document.querySelector('#rect21').style.fill = "black";
+//     document.querySelector('#rect19').style.fill = "black";
+//     document.querySelector('#rect17').style.fill = "black";
+//     document.querySelector('#rect15').style.fill = "black";
+
+
+// }
+
+// function levelTwo(){
+//     console.log('lvl 2');
+//     dreamlog.style.display = "flex";
+//     $("#dreamlog").text("log 1"); 
+//     log1.style.display = "flex";
+//     log2.style.display = "none";
+//     log3.style.display = "none";
+//     log4.style.display = "none";
+//     log5.style.display = "none";
+//     document.querySelector('#rect13').style.fill = "#c9cdc0";
+//     document.querySelector('#rect21').style.fill = "black";
+//     document.querySelector('#rect19').style.fill = "black";
+//     document.querySelector('#rect17').style.fill = "black";
+//     document.querySelector('#rect15').style.fill = "black";
+//     objective.style.display = "none";
+//     back2.style.display = "flex";
+// }
+
+// function levelThree(){
+//     dreamlog.style.display = "flex";
+//     $("#dreamlog").text("log 2");
+//     log1.style.display = "flex";
+//     log2.style.display = "flex";
+//     log3.style.display = "none";
+//     log4.style.display = "none";
+//     log5.style.display = "none";
+//     document.querySelector('#rect13').style.fill = "#c9cdc0";
+//     document.querySelector('#rect21').style.fill = "#c9cdc0";
+//     document.querySelector('#rect19').style.fill = "black";
+//     document.querySelector('#rect17').style.fill = "black";
+//     document.querySelector('#rect15').style.fill = "black";
+//     objective.style.display = "none";
+//     back2.style.display = "flex";
+
+// }
+
+// function levelFour(){
+//     dreamlog.style.display = "flex";
+//     $("#dreamlog").text("log 3");
+//     log1.style.display = "flex";
+//     log2.style.display = "flex";
+//     log3.style.display = "flex";
+//     log4.style.display = "none";
+//     log5.style.display = "none";
+//     document.querySelector('#rect13').style.fill = "#c9cdc0";
+//     document.querySelector('#rect21').style.fill = "#c9cdc0";
+//     document.querySelector('#rect19').style.fill = "#c9cdc0";
+//     document.querySelector('#rect17').style.fill = "black";
+//     document.querySelector('#rect15').style.fill = "black";
+//     objective.style.display = "none";
+//     back2.style.display = "flex";
+
+// }
+
+// function levelFive(){
+//     dreamlog.style.display = "flex";
+//     $("#dreamlog").text("log 4");
+//     log1.style.display = "flex";
+//     log2.style.display = "flex";
+//     log3.style.display = "flex";
+//     log4.style.display = "flex";
+//     log5.style.display = "none";
+//     document.querySelector('#rect13').style.fill = "#c9cdc0";
+//     document.querySelector('#rect21').style.fill = "#c9cdc0";
+//     document.querySelector('#rect19').style.fill = "#c9cdc0";
+//     document.querySelector('#rect17').style.fill = "#c9cdc0";
+//     document.querySelector('#rect15').style.fill = "black";
+//     objective.style.display = "none";
+//     back2.style.display = "flex";
+
+// }
+
+// function levelSix(){
+//     dreamlog.style.display = "flex";
+//     $("#dreamlog").text("log 5");
+//     log1.style.display = "flex";
+//     log2.style.display = "flex";
+//     log3.style.display = "flex";
+//     log4.style.display = "flex";
+//     log5.style.display = "flex";
+//     document.querySelector('#rect13').style.fill = "#c9cdc0";
+//     document.querySelector('#rect21').style.fill = "#c9cdc0";
+//     document.querySelector('#rect19').style.fill = "#c9cdc0";
+//     document.querySelector('#rect17').style.fill = "#c9cdc0";
+//     document.querySelector('#rect15').style.fill = "#c9cdc0";
+//     objective.style.display = "none";
+//     back2.style.display = "flex";
+
+// }
+
+
